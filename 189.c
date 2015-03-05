04394ee7e688a8ccbc7d32b4e0667b58ea9b41af
lennylxx <lennylxx@gmail.com>
2015-03-05 16:47:24 +0800

    "Rotate Array"

---
+++ 189.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void rotate(int nums[], int n, int k) {
+    int i, j;
+    i = j = 0;
+    int x = nums[i];
+    int t;
+    for(int c = 0; c < n; c++)
+    {
+        i = (i + k) % n;
+        t = nums[i];
+        nums[i] = x;
+        x = t;
+        if (i == j)
+        {
+            i = ++j;
+            x = nums[i];
+        }
+    }
+}
+
+void print(int nums[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int a[7] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7;
+    int k = 4;
+
+    rotate(a, n, k);
+    print(a, n);
+
+    int b[6] = {1, 2, 3, 4, 5, 6};
+    n = 6;
+    k = 3;
+
+    rotate(b, n, k);
+    print(b, n);
+
+    return 0;
+}
