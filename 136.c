0179d65bd8fb2a0dfe31c82a4015904d566cfb38
lennylxx <lennylxx@gmail.com>
2015-03-05 20:39:04 +0800

    "Single Number"

---
+++ 136.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int singleNumber(int A[], int n) {
+    int one = 0;
+    for (int i = 0; i < n; i++) {
+        one = one ^ A[i];
+    }
+    return one;
+}
+
+int main()
+{
+    int A[] = {1, 2, 1, 3, 4, 3, 4};
+    printf("%d\n", singleNumber(A, sizeof(A)/sizeof(A[0])));
+    return 0;
+}
