9750bff3ab84c520478b315cd5e02f112fcf9f1f
lennylxx <lennylxx@gmail.com>
2015-03-13 21:18:59 +0800

    "Best Time to Buy and Sell Stock II": another method

--- 122.c
+++ 122.c
@@ -11,6 +11,24 @@ int maxProfit(int prices[], int n){
     return ret;
 }
 
+/*
+int maxProfit(int prices[], int n){
+    int i;
+    int ret = 0;
+    int min = prices[0];
+    for (i = 1; i < n; i++) {
+        if (prices[i] < prices[i - 1]) {
+            ret += prices[i - 1] - min;
+            min = prices[i];
+        }
+    }
+    if (prices[n - 1] > min)
+        ret += prices[n - 1] - min;
+    
+    return ret;
+}
+*/
+
 int main() {
     int prices[] = {1, 2, 3, 2, 4};
     printf("%d\n", maxProfit(prices, sizeof(prices)/sizeof(prices[0])));
