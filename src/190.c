311e66020ae2063bf739051ed1aadee26ef0f6d6
lennylxx <lennylxx@gmail.com>
2015-03-25 11:57:47 +0800

    "Reverse Bits": simplification

--- src/190.c
+++ src/190.c
@@ -1,20 +1,4 @@
 #include <stdio.h>
-#include <stdint.h>
-
-/*
-uint32_t reverseBits(uint32_t n) {
-    uint32_t ret = 0;
-    int i = 31;
-    while (i--) { // 31 times
-        ret += n & 0x01;
-        n >>= 1;
-        ret <<= 1;
-    }
-    ret += n & 0x01;
-
-    return ret;
-}
-*/
 
 void print_bits(uint32_t n) {
     int i = 32;
@@ -27,7 +11,22 @@ void print_bits(uint32_t n) {
     printf("\n");
 }
 
-uint32_t reverseBits(uint32_t n) {
+/* 32 steps */
+uint32_t reverseBits_1(uint32_t n) {
+    uint32_t ret = 0;
+    int i = 31;
+    while (i--) { // 31 times
+        ret += n & 0x01;
+        n >>= 1;
+        ret <<= 1;
+    }
+    ret += n & 0x01;
+
+    return ret;
+}
+
+/* 5 steps : 5 * lg(N) operations */
+uint32_t reverseBits_2(uint32_t n) {
     uint32_t t = n;
     /* swap odd and even bits  1 bit*/
     t = ((t & 0xAAAAAAAA) >> 1) | ((t & 0x55555555) << 1);
@@ -41,6 +40,16 @@ uint32_t reverseBits(uint32_t n) {
     t = ((t & 0xFFFF0000) >> 16) | ((t & 0x0000FFFF) << 16);
     
     return t;
+}
+
+/* simplification of reverseBits_2 */
+uint32_t reverseBits(uint32_t n) {
+    n = ((n >> 1) & 0x55555555) | ((n & 0x55555555) << 1);
+    n = ((n >> 2) & 0x33333333) | ((n & 0x33333333) << 2);
+    n = ((n >> 4) & 0x0F0F0F0F) | ((n & 0x0F0F0F0F) << 4);
+    n = ((n >> 8) & 0x00FF00FF) | ((n & 0x00FF00FF) << 8);
+    n = ( n >> 16             ) | ( n               << 16);
+    return n;
 }
 
 int main() {
