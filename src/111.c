83e71a372e330474d4e74d38ba502af3743f5de2
lennylxx <lennylxx@gmail.com>
2015-03-29 14:45:42 +0800

    "Minimum Depth of Binary Tree"

---
+++ src/111.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int minDepth(struct TreeNode *root) {
+    if (root == NULL)
+        return 0;
+    
+    int l = minDepth(root->left);
+    int r = minDepth(root->right);   
+    
+    if (l && r) {
+        if (l < r)
+            return l + 1;
+        else
+            return r + 1;
+    }
+    else if (l == 0 && r == 0) { /* leaf */
+        return 1;
+    }
+    else {
+        if (l == 0) /* no left child */
+            return r + 1;
+        if (r == 0) /* no right child */
+            return l + 1;
+    }
+}
+
+void printTreePreOrder(struct TreeNode *p) {
+    if (p != NULL) {
+        printf("%d", p->val);
+        printTreePreOrder(p->left);
+        printTreePreOrder(p->right);
+    }
+    else printf("#");
+}
+
+int main() {
+    struct TreeNode *t = (struct TreeNode *)calloc(4, sizeof(struct TreeNode));
+    struct TreeNode *p = t;
+    p->val = 1;
+    p->right = ++t;
+    t->val = 2;
+    t->right = NULL;
+    p->right->left = ++t;
+    t->val = 3;
+    t->left = NULL;
+    p->right->left->right = ++t;
+    t->val = 4;
+    t->left = t->right = NULL;
+    printTreePreOrder(p); printf("\n");
+
+    /* should be 4 */
+    printf("%d\n", minDepth(p));
+
+    return 0;
+}
