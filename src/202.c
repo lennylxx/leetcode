06745f4717cf4d26366238c2a8483409d9b2648d
lennylxx <lennylxx@gmail.com>
2015-04-23 14:22:02 +0800

    "Happy Number": a simpler method

--- src/202.c
+++ src/202.c
@@ -12,10 +12,12 @@ static inline int hash(int key, int size) {
     return (index > 0) ? (index) : (-index);
 }
 
-bool isHappy(int n) {
+bool isHappy_1(int n) {
     if (n == 1) return true;
 
-    int size = 128;
+    int i;
+    bool isHappy = false;
+    int size = 20;
     struct HashNode **hashtable
         = (struct HashNode **)calloc(size, sizeof(struct HashNode*));
 
@@ -25,7 +27,8 @@ bool isHappy(int n) {
         struct HashNode **p = hashtable + index;
         while (*p){
             if ((*p)->key == n) { /* found, endless loop, n is not happy */
-                return false;
+                isHappy = false;
+                goto OUT;
             }
             p = &((*p)->next);
         }
@@ -42,8 +45,46 @@ bool isHappy(int n) {
             n /= 10;
         }
 
-        if (t == 1) return true;
+        if (t == 1) {
+            isHappy = true;
+            goto OUT;
+        }
         else n = t;
+    }
+
+OUT:
+    /* free memory */
+    for (i = 0; i < size; i++) {
+        struct HashNode *t = hashtable[i];
+        struct HashNode *x = NULL;
+        while (t) {
+            x = t->next;
+            free(t);
+            t = x;
+        }
+    }
+    free(hashtable);
+
+    return isHappy;
+}
+
+/**
+ * If a number is NOT happy, there is always a 4 in the cycle.
+ * https://en.wikipedia.org/wiki/Happy_number#Sequence_behavior
+ */
+bool isHappy(int n) {
+    if (n <= 0) return false;
+    
+    int magic = 4;
+    while (1) {
+        if (n == 1) return true;
+        if (n == magic) return false;
+        int t = 0;
+        while (n) {
+            t += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        n = t;
     }
 }
 
