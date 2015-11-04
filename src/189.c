cf5dbd3fc296a2719331ce85c025c560c3ce5aa8
lennylxx <lennylxx@gmail.com>
2015-11-04 17:40:57 +0800

    "Rotate Array": cyclic group generator

--- src/189.c
+++ src/189.c
@@ -3,18 +3,39 @@
 void rotate(int nums[], int n, int k) {
     int i, j, c;
     i = j = 0;
-    int x = nums[i];
+    int pre = nums[i];
     int t;
-    for(c = 0; c < n; c++)
-    {
+    for(c = 0; c < n; c++) {
         i = (i + k) % n;
         t = nums[i];
-        nums[i] = x;
-        x = t;
-        if (i == j)
-        {
+        nums[i] = pre;
+        pre = t;
+        if (i == j) {
             i = ++j;
-            x = nums[i];
+            pre = nums[i];
+        }
+    }
+}
+
+/*
+ Cyclic group generator to do matrix transpose.
+ use a 1-dimension array to reprents m*n matrix.
+ the result is an n*m matrix.
+ */
+
+void transpose(int *matrix, int m, int n) {
+    int i, j, c;
+    i = j = 0;
+    int pre = matrix[0];
+    int t;
+    for (c = 0; c < m * n - 1; c++) { /* pay attention it's m*n-1 */
+        i = (i % n) * m + (i / n);
+        t = matrix[i];
+        matrix[i] = pre;
+        pre = t;
+        if (i == j) {
+            i = ++j;
+            pre = matrix[i];
         }
     }
 }
